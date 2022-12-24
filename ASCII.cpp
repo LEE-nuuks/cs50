a2c82fc95c7b0cac4e5fd94f0e4adc40c9b092cb
cpp <109072496+LEE-nuuks@users.noreply.github.com>
2022-12-24 18:27:20 +0600

    Update ASCII.cpp

--- ASCII.cpp
+++ ASCII.cpp
@@ -4,5 +4,45 @@ using namespace std;
 
 int main()
 {
-  return 0;
+    // character to decimal
+    
+    
+    // decimal to character
+    
+    // method::1
+    int num = 49;
+    char ch = num;
+    cout << ch;
+    
+    cout << char(32); // space
+    
+    
+    // method::2
+    int ascii = 43;
+    cout << char(ascii);
+    
+    cout << char(32); // space
+    
+    
+    // method::3
+    int num2 = 50;
+    cout << static_cast<char>(num2);
+    
+    cout << char(32); // space
+    
+    
+    // method::4
+    int equ = 61;
+    cout << (char) equ;
+    
+    cout << char(32); // space
+    
+    
+    int res = 51;
+    cout << static_cast<char>(res);
+
+
+    return 0;
 }
+
+// ref:: ascii table: https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
