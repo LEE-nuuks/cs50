056f3dcbd262322da1a1f88c71af6a88f5a3df7f
cpp <109072496+LEE-nuuks@users.noreply.github.com>
2022-12-24 19:18:43 +0600

    Update ASCII.cpp

--- ASCII.cpp
+++ ASCII.cpp
@@ -2,6 +2,118 @@
 
 using namespace std;
 
+// v1
+void myKeyboard() {
+    // calculator with ascii
+    int ch1, ch2;
+    cout << "ch1: "; // get ascii value from the user
+    cin >> ch1;
+    cout << "ch2: "; // get ascii value from the user
+    cin >> ch2;
+    
+    // user give that ascii decimal we use it as our variable name of our program
+    int n1 = char(ch1);
+    int n2 = char(ch2);
+    
+    cout << "n1: ";
+    cout << n1;
+    cin >> n1;
+    cout << "n2: ";
+    cin >> n2;
+    
+    char opertion;
+    cout << "op: ";
+    cin >> opertion;
+    
+    int space;
+    cout << "space? \n\t1. yes\n\t2.no" << endl;
+    cin >> space;
+    if(space == 1){
+        space = 32;
+    }
+    if(static_cast<int>(opertion) == 43)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 + n2;
+    }
+    else if(static_cast<int>(opertion) == 45)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 - n2;
+    }
+    else if(static_cast<int>(opertion) == 42)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 * n2;
+    }
+    else if(static_cast<int>(opertion) == 47)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 / n2;
+    }
+    
+}
+
+
+// v2
+void asciiCAL() {
+    
+    // calculator with ascii
+    
+    int ch1, ch2;
+    cout << "ch1: ";
+    cin >> ch1;
+    cout << "ch2: ";
+    cin >> ch2;
+    
+    // int n1 = char(ch1);
+    // int n2 = char(ch2);
+    int n1,  n2;
+    
+    cout << char(ch1) << "= ";
+    cin >> n1;
+    cout << char(ch2) << "= ";
+    cin >> n2;
+    
+    int choose;
+    char opertion;
+    cout << "select the right opertion: ";
+    cout << "\n\t1. +\n\t2. -\n\t3. *\n\t4. /\n";
+    cin >> choose;
+    if(choose == 1) {
+        opertion = '+';
+    }
+    else if (choose == 2) {
+        opertion = '-';
+    }
+    else if (choose == 3) {
+        opertion = '*';
+    }
+    else if (choose == 4) {
+        opertion = '/';
+    }
+    
+    int space;
+    cout << "space? \n\t1. yes\n\t2.no" << endl;
+    cin >> space;
+    if(space == 1){
+        space = 32;
+    }
+    if(static_cast<int>(opertion) == 43)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 + n2;
+    }
+    else if(static_cast<int>(opertion) == 45)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 - n2;
+    }
+    else if(static_cast<int>(opertion) == 42)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 * n2;
+    }
+    else if(static_cast<int>(opertion) == 47)
+    {
+        cout << char(ch1) << static_cast<char>(space) << opertion << static_cast<char>(space) << char(ch2) << static_cast<char>(space) << char(61) << static_cast<char>(space) << n1 / n2;
+    }
+    
+}
+
 // convert ascii code >> (character>decimal) (decimal to character)
 
 int main()
